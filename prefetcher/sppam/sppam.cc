a98275a497a446eb451f5125607da7c185103df2
Shivang Bharti <shivangbharti@olympus-login.engr.tamu.edu>
2026-04-06 20:39:37 -0500

    sppam update

--- prefetcher/sppam/sppam.cc
+++ prefetcher/sppam/sppam.cc
@@ -1,11 +1,106 @@
 #include "sppam.h"
-
+uint64_t access_counter = 0;
 uint32_t sppam::prefetcher_cache_operate(champsim::address addr, champsim::address ip, uint8_t cache_hit, bool useful_prefetch, access_type type,
-                                      uint32_t metadata_in)
+                                         uint32_t metadata_in)
 {
   // assert(addr == ip); // Invariant for instruction prefetchers
+  uint64_t curr_offset = (addr >> log2(64)) & (0x3F); // 3F--63 --blovk offset from 0 to 63
+  uint64_t page = addr >> 12;                          // 12 bits are offset
+  int16_t delta;                                       // int --can be -ve too
+  bool hit_status = false;
+  uint16_t old_sig =0;
+  bool lru_entry_hit = false;
+  uint32_t replacement_entry;
+  bool invalid_entry_hit = false;
+  uint64_t final_hit_index=0;
+
+  for (int i = 0; i < 255; i++) {
+    if (page == s_table.Sig_table[i].tag && s_table.Sig_table[i].valid == true) // hit
+    {
+      delta = curr_offset - s_table.Sig_table[i].last_offset;
+      old_sig = s_table.Sig_table[i].sig;
+      s_table.Sig_table[i].sig = (s_table.Sig_table[i].sig << 3) ^ (delta);
+      s_table.Sig_table[i].last_offset = delta;
+      s_table.Sig_table[i].lru_last_used = access_counter;
+      s_table.Sig_table[i].valid = true;
+      hit_status = true;
+      final_hit_index=i;
+    }
+  }
+  uint64_t lru_min_counter = UINT64_MAX;
+  if (!hit_status) // miss
+  {
+    for (int j = 0; j < 255; j++) {
+      if (s_table.Sig_table[j].valid == false) {
+        replacement_entry = j;
+        invalid_entry_hit = true;
+        break;
+      }
+    }
+
+    if (invalid_entry_hit == false) // Ssearch for lru entry
+    {
+      for (int j = 0; j < 255; j++) {
+        if (s_table.Sig_table[j].lru_last_used < lru_min_counter) {
+          lru_min_counter = s_table.Sig_table[j].lru_last_used;
+          replacement_entry=j;
+          lru_entry_hit=true;
+        }
+      }
+    }
+
+    //add or replace the entry
+      old_sig=0;
+      delta=0;
+      s_table.Sig_table[replacement_entry].tag = page;
+      s_table.Sig_table[replacement_entry].sig = 0;
+      s_table.Sig_table[replacement_entry].last_offset = curr_offset;
+      s_table.Sig_table[replacement_entry].lru_last_used = access_counter;
+      s_table.Sig_table[replacement_entry].valid = true;
+  
+    final_hit_index=replacement_entry;
+  }
+  update_pt(old_sig, delta);
+
+  uint16_t curr_sig =s_table.Sig_table[final_hit_index].sig;
+  prefetch_gen (); // to be implemented
+
   return metadata_in;
 }
+
+void sppam::update_pt(uint16_t sig, int8_t delta)
+{
+
+  /// To be implemented: if either c_delta or c_sig saturates, right-shift all counters in that entry by 1 instead of clamping
+  uint16_t index;
+  index = sig & 0x1FF;
+  bool c_delta_match=false;
+  uint16_t index_low_delta;
+  uint16_t lowest_delta_value = 16;
+  //inc c_sig
+  p_table.pattern[index].c_sig = (p_table.pattern[index].c_sig>14) ? 15 : (p_table.pattern[index].c_sig + 1);   
+  //inc c_delta
+  for (int i=0;i<3;i++)
+  {
+    if(p_table.pattern[index].predictors[i].c_delta==delta)//match
+    {
+      p_table.pattern[index].predictors[i].c_delta = (p_table.pattern[index].predictors[i].c_delta>14) ? 15 : (p_table.pattern[index].predictors[i].c_delta + 1); 
+      c_delta_match=true;
+    }
+    if(p_table.pattern[index].predictors[i].c_delta<lowest_delta_value)
+    {
+      index_low_delta=i;
+    }
+
+  }
+  if (c_delta_match==false) //replacing the lowest delta entry
+  {
+    p_table.pattern[index].predictors[index_low_delta].c_delta = 1;
+    
+    p_table.pattern[index].predictors[index_low_delta].delta = delta;
+  }
+}
+
 
 uint32_t sppam::prefetcher_cache_fill(champsim::address addr, long set, long way, uint8_t prefetch, champsim::address evicted_addr, uint32_t metadata_in)
 {
--- prefetcher/sppam/sppam.h
+++ prefetcher/sppam/sppam.h
@@ -10,12 +10,75 @@ struct sppam : public champsim::modules::prefetcher {
   using prefetcher::prefetcher;
   uint32_t prefetcher_cache_operate(champsim::address addr, champsim::address ip, uint8_t cache_hit, bool useful_prefetch, access_type type,
                                     uint32_t metadata_in);
-  uint32_t prefetcher_cache_fill(champsim::address addr, long set, long way, uint8_t prefetch, champsim::address evicted_addr, uint32_t metadata_in);
+  // This function is called when a tag is checked in the cache. The parameters passed are:
+  // addr: the address of the packet. If this is the first-level cache, the offset bits are included. Otherwise, the offset bits are zero. If the cache was
+  // configured with “virtual_prefetch”: true, this address will be a virtual address. Otherwise, this is a physical address. ip: the address of the instruction
+  // that initiated the demand. If the packet is a prefetch from another level, this value will be 0. cache_hit: if this tag check is a hit, this value is
+  // nonzero. Otherwise, it is 0. useful_prefetch: if this tag check hit a prior prefetch, this value is true. type: the result of
+  // static_cast<std::underlying_type_t<access_type>>(v) for v in: * access_type::LOAD * access_type::RFO * access_type::PREFETCH * access_type::WRITE *
+  // access_type::TRANSLATION metadata_in: the metadata carried along by the packet. The function should return metadata that will be stored alongside the
+  // block.
 
-  // void prefetcher_initialize();
+  uint32_t prefetcher_cache_fill(champsim::address addr, long set, long way, uint8_t prefetch, champsim::address evicted_addr, uint32_t metadata_in);
+  // This function is called when a miss is filled in the cache. The parameters passed are:
+  // addr: the address of the packet. If this is the first-level cache, the offset bits are included. Otherwise, the offset bits are zero. If the cache was
+  // configured with “virtual_prefetch”: true, this address will be a virtual address. Otherwise, this is a physical address. set: the set that the fill
+  // occurred in way: the way that the fill occurred in, or this->NUM_WAY if a bypass occurred prefetch: if this tag check hit a prior prefetch, this value is
+  // true. metadata_in: the metadata carried along by the packet. The function should return metadata that will be stored alongside the block.
+  void prefetcher_initialize();
+  // This function is called when the cache is initialized.
+  // You can use it to initialize elements of dynamic structures, such as std::vector or std::map.
+
   // void prefetcher_branch_operate(champsim::address ip, uint8_t branch_type, champsim::address branch_target) {}
   // void prefetcher_cycle_operate() {}
-  // void prefetcher_final_stats() {}
+  void prefetcher_final_stats() {} // This function is called at the end of the simulation and can be used to print statistics.
+
+  struct Sig_row {
+    uint64_t tag=0;
+    uint16_t sig=0;
+    uint16_t last_offset=0;
+    uint16_t lru_last_used=0; // 0 to 255
+    bool valid=false;
+  };
+
+
+  class signature_table
+  {
+  public:
+    Sig_row Sig_table[256];
+
+    signature_table() // constructor
+    {
+      for (uint32_t i = 0; i < 255; i++) {
+        Sig_table->valid = false;
+        Sig_table->sig = 0;
+        Sig_table->lru_last_used = 0;
+      }
+    }
+  };
+
+  
+  struct Pattern_Table_entry {
+    struct Deltas {
+      int8_t delta = 0;
+      uint8_t c_delta = 0;
+    } predictors[4];
+    uint8_t c_sig = 0;
+  };
+
+  class pattern_table
+  {
+  public:
+    Pattern_Table_entry pattern[512]; // 12 bit signature
+
+  protected:
+  };
+  
+  
+  void  update_pt(uint16_t sig, int8_t delta);
+  void prefetch_gen();
+  signature_table s_table;
+  pattern_table p_table;
 };
 
 #endif
